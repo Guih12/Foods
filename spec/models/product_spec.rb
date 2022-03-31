5b4f79c09692b8f09f6cc9f886df9fc108f887aa
George Borsato <georginhoborsato@gmail.com>
2022-03-31 15:56:52 -0300

    test: create test validation attribute when is not valid

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    subject(:product) { create(:product) }
+
+    context "when invalid attributes" do
+      it "is not valid attribute name" do
+        product.name = nil
+        expect(product).to_not be_valid
+      end
+
+      it "is not valid attribute description" do
+        product.description = nil
+        expect(product).to_not be_valid
+      end
+
+      it "is not valid attribute type production" do
+        product.type_product = nil
+        expect(product).to_not be_valid
+      end
+
+      it "is not valid attribute price" do
+        product.price = nil
+        expect(product).to_not be_valid
+      end
+    end
+  end
 end
