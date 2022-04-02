ba5e72a56987632c0ba77549f7809c7db7c86f51
George Borsato <georginhoborsato@gmail.com>
2022-04-02 11:42:34 -0300

    test: create content error

--- spec/use_cases/combo/create_spec.rb
+++ spec/use_cases/combo/create_spec.rb
@@ -10,14 +10,29 @@ RSpec.describe Combos::Create do
       "name": combo.name,
       "description": combo.description,
       "price": combo.price,
-      "combo_items_attributes": [{"product_id": product.id}]
+      "combo_items_attributes": [{ "product_id": product.id }]
     }
   end
 
   describe '.persist' do
-    context "when valid params" do
+    context 'when valid params' do
       it 'create combo' do
         expect(subject).to eq subject
+      end
+    end
+
+    context 'when invalid params' do
+      let(:combo_attributes) do
+        {
+          "name": nil,
+          "description": nil,
+          "price": combo.price,
+          "combo_items_attributes": [{ "product_id": product.id }]
+        }
+      end
+      it 'return message errors' do
+        expect(subject.messages).to have_key(:name)
+        expect(subject.messages).to have_key(:description)
       end
     end
   end
