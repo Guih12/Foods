fddb898237cd0729a8db815eab30d07275f6f119
George Borsato <georginhoborsato@gmail.com>
2022-04-06 18:32:47 -0300

    feat: create use case discount place_order

---
+++ app/use_cases/place_orders/place_order_calculate_discount.rb
@@ -0,0 +1,52 @@
+module PlaceOrders
+  class PlaceOrderCalculateDiscount
+    def initialize(place_order)
+      @place_order = place_order
+    end
+
+    PERCENTS = {
+      twenteen: 0.17,
+      teen: 0.10,
+      five: 0.05
+    }
+
+
+    def calculate_discount
+      return discount_twenteen_percent if verification_combo_and_lanche
+      return discount_teen_percent if verification_combo
+      return discount_five_percent if verification_lanche
+
+    end
+
+
+    def verification_combo_and_lanche
+      verification_combo && verification_lanche
+    end
+
+    def verification_combo
+      place_order.combos.count >= 2 ? true : false
+    end
+
+    def verification_lanche
+      place_order.products.count >= 2 ? true : false
+    end
+
+
+    private
+
+    attr_reader :place_order
+
+    def discount_twenteen_percent
+      PERCENTS[:twenteen]
+    end
+
+    def discount_teen_percent
+      PERCENTS[:teen]
+    end
+
+    def discount_five_percent
+      PERCENTS[:five]
+    end
+
+  end
+end
