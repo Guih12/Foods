8f7c4f67c7167d1d2615b00a9a00ee7ef2bb8cfc
George Borsato <georginhoborsato@gmail.com>
2022-04-07 09:20:41 -0300

    test: create test unit calculate price

---
+++ spec/use_cases/place_orders/calculate_price_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe PlaceOrders::CalculatePrice do
+
+  let(:place_order) { create(:place_order) }
+  let(:product) { create(:product) }
+  let(:product_two) { create(:product) }
+
+  let(:combo) { create(:combo) }
+  let(:combo_two) { create(:combo) }
+
+
+  describe '.calculate_total' do
+    let(:calculate_price) { PlaceOrders::CalculatePrice.new(place_order) }
+    let(:total_combo) { place_order.combos.map(&:price).sum }
+    let(:total_products) { place_order.products.map(&:price).sum.to_f }
+    let(:price_total) { total_combo + total_products }
+
+    before do
+      place_order.place_order_combo_items_attributes = [{ combo_id: combo.id }]
+      place_order.place_order_combo_items_attributes = [{ combo_id: combo_two.id }]
+
+      place_order.place_order_product_items_attributes = [{ product_id: product.id }]
+      place_order.place_order_product_items_attributes = [{ product_id: product_two.id }]
+
+      place_order.save
+    end
+
+    it 'return value 8' do
+      expect(calculate_price.calculate_total).to eq price_total
+    end
+  end
+
+  describe '.calculate_with_discount' do
+    let(:calculate_price) { PlaceOrders::CalculatePrice.new(place_order) }
+    let(:total_combo) { place_order.combos.map(&:price).sum }
+    let(:total_products) { place_order.products.map(&:price).sum.to_f }
+    let(:price_total) { total_combo + total_products }
+    let(:calculate_discount) { PlaceOrders::CalculateDiscount.new(place_order).calculate_discount }
+
+    let(:price_with_discount) { price_total - (price_total * calculate_discount) }
+
+    before do
+      place_order.place_order_combo_items_attributes = [{ combo_id: combo.id }]
+      place_order.place_order_combo_items_attributes = [{ combo_id: combo_two.id }]
+
+      place_order.place_order_product_items_attributes = [{ product_id: product.id }]
+      place_order.place_order_product_items_attributes = [{ product_id: product_two.id }]
+
+      place_order.save
+    end
+
+    it 'return price with discount 6.64' do
+      expect(calculate_price.calculate_price_with_discount).to eq price_with_discount
+    end
+  end
+end
