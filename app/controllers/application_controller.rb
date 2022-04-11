dff86c87840fefa8dca2b70af10a14be78abfd66
George Borsato <georginhoborsato@gmail.com>
2022-04-11 13:40:28 -0300

    style: fix lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::API
 
   before_action :configure_sign_up_params, if: :devise_controller?
 
-
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :age, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[name lastname age email password password_confirmation])
   end
 end
