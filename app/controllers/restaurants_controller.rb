88c32e0ef4273e2f21fdc2f4d3b00ecd2eead458
George Borsato <georginhoborsato@gmail.com>
2022-04-11 12:08:19 -0300

    feat: include methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,18 +3,18 @@ class RestaurantsController < ApplicationController
 
 
   def index
-    render json: {ok: 'ok'}, status: 200
+    render json: RestaurantSerializer.new(collection).serialized_json, status: 200
   end
 
   def show
-    render json: {ok: 'ok'}, status: 200
+    render json: RestaurantSerializer.new(restaurant).serialized_json, status: 200
   end
 
 
   def create
     restaurant = Restaurants::Create.new(restaurant_attributes).persist
     if !restaurant.errors.present?
-      render json: restaurant, status: 201
+      render json: RestaurantSerializer.new(restaurant).serialized_json, status: 201
     else
       render json: restaurant, status: 422
     end
@@ -23,7 +23,16 @@ class RestaurantsController < ApplicationController
 
   private
 
+  def collection
+    @collection ||= ::Restaurant.all
+  end
+
+  def restaurant
+    @restaurant ||= ::Restaurant.find(params[:id])
+  end
+
   def restaurant_attributes
     params.require(:restaurant).permit(:name, :number_phone, :user_id)
   end
+
 end
