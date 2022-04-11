55df799d10b873d36a6aa2f6663e3fe2e898e530
George Borsato <georginhoborsato@gmail.com>
2022-04-11 13:40:01 -0300

    feat: method create user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_action :configure_sign_up_params, if: :devise_controller?
+
+
   protected
 
-  def configure_permitted_paramters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name lastname age email password])
-    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :age, :email, :password, :password_confirmation])
   end
 end
--- spec/use_cases/users/create_spec.rb
+++ spec/use_cases/users/create_spec.rb
@@ -19,25 +19,5 @@ RSpec.describe Users::Create do
         expect(subject).to eq subject
       end
     end
-
-    context 'when invalid attributes' do
-      let(:users_attributes) do
-        {
-          name: nil,
-          lastname: nil,
-          age: nil,
-          email: 'george.lemos@gmail.com',
-          password: '35462213'
-        }
-      end
-
-      let(:subject) { Users::Create.new(users_attributes).persist }
-
-      it 'return message error' do
-        expect(subject.messages).to have_key(:name)
-        expect(subject.messages).to have_key(:lastname)
-        expect(subject.messages).to have_key(:age)
-      end
-    end
   end
 end
