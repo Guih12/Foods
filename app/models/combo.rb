fecb52ba77fcde5d68b734055212c163b6ebb898
George Borsato <georginhoborsato@gmail.com>
2022-04-11 15:46:47 -0300

    feat: include relation restaurant

--- app/models/combo.rb
+++ app/models/combo.rb
@@ -1,6 +1,7 @@
 class Combo < ApplicationRecord
   has_many :combo_items
   has_many :products, through: :combo_items
+  belongs_to :restaurant
 
   validates_presence_of :name, :description, presence: true
   accepts_nested_attributes_for :combo_items, allow_destroy: true
