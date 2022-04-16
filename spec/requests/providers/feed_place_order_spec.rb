40dfc24b02aa62876d62fab984497d1155d4d059
George Borsato <georginhoborsato@gmail.com>
2022-04-16 17:35:28 -0300

    test: include test request unit

---
+++ spec/requests/providers/feed_place_order_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Providers::FeedPlaceOrdersController do
+  let(:user) { create(:user) }
+  let(:auth_headers) { user.create_new_auth_token }
+  let(:place_order) { create(:place_order) }
+
+  context 'when user is looged' do
+    describe 'GET /providers/feed_place_orders' do
+      before(:each) { get '/providers/feed_place_orders', headers: auth_headers}
+
+      it 'return status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'GET /providers/feed_place_orders/:id' do
+      before(:each) { get "/providers/feed_place_orders/#{place_order.id}", headers: auth_headers }
+
+      it 'return status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  context 'when user is not logged' do
+    describe 'GET /providers/feed_place_orders' do
+      before(:each) { get '/providers/feed_place_orders' }
+
+      it 'return status code 401' do
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    describe 'GET /providers/feed_place_orders/:id' do
+      before(:each) { get "/providers/feed_place_orders/#{place_order.id}" }
+
+      it 'return status code 401' do
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
+end
